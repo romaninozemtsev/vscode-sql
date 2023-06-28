14f5755fa867207e20ea51f9b1e4506cec7f4b9c
Roman Inozemtsev <roman.inozemtsev+github@gmail.com>
2023-06-27 22:38:38 -0700

    adding notebook traces

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,10 @@
 import * as vscode from 'vscode';
 import { DatabaseProvider } from './tree-view';
 import { establishConnection } from './db-conn';
+import { SampleContentSerializer } from './serializer';
+import { SampleKernel } from './controller';
+
+const NOTEBOOK_TYPE = 'sql-notebook';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -23,13 +27,46 @@ export async function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from vscode-sql!');
 	});
 
+	let addConnectionCommand =  vscode.commands.registerCommand('vscode-sql.addConnection', () => {
+		vscode.window.showInformationMessage('add connection');
+	});
+
 	// create tree view with DatabaseProvider as data provider
 
 	vscode.window.createTreeView('databaseExplorer', {
 		treeDataProvider: new DatabaseProvider(connection)
 	});
 
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-sql.createSqlNotebook', async () => {
+		const language = 'sql';
+		const defaultValue = `select * from users`;
+		const cell = new vscode.NotebookCellData(vscode.NotebookCellKind.Code, defaultValue, language);
+		const data = new vscode.NotebookData([cell]);
+		data.metadata = {
+			custom: {
+				cells: [],
+				metadata: {
+					orig_nbformat: 4
+				},
+				nbformat: 4,
+				nbformat_minor: 2
+			}
+		};
+		const doc = await vscode.workspace.openNotebookDocument(NOTEBOOK_TYPE, data);
+		await vscode.window.showNotebookDocument(doc);
+	}));
+
+	context.subscriptions.push(
+		vscode.workspace.registerNotebookSerializer(
+			NOTEBOOK_TYPE, new SampleContentSerializer(), { transientOutputs: true }
+		),
+		new SampleKernel()
+	);
+
+
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(addConnectionCommand);
 }
 
 // This method is called when your extension is deactivated
